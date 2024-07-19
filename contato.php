54bc957c22140556589422a9ec19837d05ab6837
APARECIDO ALVES DE SOUZA <aparecido.4864@aluno.pr.senac.br>
2024-07-19 16:31:06 -0300

    msg

---
+++ contato.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-pr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+     <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="container text-center">
+
+        
+    </div>
+
+    <div class="container">
+        <form action="" method="get">
+
+        <label for="">Nome</label>
+        <input class="form-control" type="text">
+        <label for="">email</label>
+        <input class="form-control" type="text">
+        <label for="">Mensagem</label>
+        <input class="form-control" type="text">
+        
+
+            <input class="btn btn-success" type="submit">
+            <input class="btn btn-danger"type="reset">
+        </form>
+    </div>
+    
+        
+</body>
+</html>
